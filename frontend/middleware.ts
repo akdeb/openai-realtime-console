13ccb6f41ca542aab88e1870b7e722fccf63a04d
akdeb <deb2@illinois.edu>
2025-04-20 13:27:09 +0100

    fix middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -2,17 +2,6 @@ import { NextResponse, type NextRequest } from "next/server";
 import { updateSession } from "@/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  const hostname = request.headers.get('host')?.split(':')[0] || '';
-  const pathname = request.nextUrl.pathname;
-
-  // Only rewrite '/' for labs.elatoai.com
-  if (hostname === 'labs.elatoai.com' && pathname === '/') {
-    const url = request.nextUrl.clone();
-    url.pathname = '/labs';
-    return NextResponse.rewrite(url);
-  }
-
-  
   return await updateSession(request);
 }
 
